da8f04a7e2c9b5419594e621c87b0e1a1e0d1562
ayushmukho <ayushmukho@gmail.com>
2022-01-14 20:06:41 +0530

    Boiler Plate Code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// import { Button } from "@chakra-ui/react";
+import Homepage from "./Pages/Homepage";
+import ChatPage from "./Pages/ChatPage";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
