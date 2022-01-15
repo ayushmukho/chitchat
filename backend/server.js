38f031a8b9e195dd571030be4b60d8d8258c8bd9
ayushmukho <ayushmukho@gmail.com>
2022-01-15 14:26:20 +0530

    Auth flow --backend

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,20 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 
+const userRoutes = require("./routes/userRoutes")
+
 dotenv.config();
+
 connectDB()
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find(c => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 5000
 
