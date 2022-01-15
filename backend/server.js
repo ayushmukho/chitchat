03e1f413dc602997039d0433bf31a9af86254597
ayushmukho <ayushmukho@gmail.com>
2022-01-15 19:07:13 +0530

    Error Handling in backend

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,26 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
-connectDB()
+connectDB();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT || 5000
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server Running on Port ${PORT}`.yellow.bold));
