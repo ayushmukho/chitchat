414def6267af806a94938cfb70283b30d735866a
ayushmukho <ayushmukho@gmail.com>
2022-01-15 13:19:57 +0530

    Connected to MONGO DB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 dotenv.config();
+connectDB()
 const app = express();
 
 app.get("/", (req, res) => {
@@ -20,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server Running on Port ${PORT}`));
+app.listen(PORT, console.log(`Server Running on Port ${PORT}`.yellow.bold));
